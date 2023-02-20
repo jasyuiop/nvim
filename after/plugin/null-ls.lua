f708fb473ee4b854f86763000ff8c53b623a36a7
M.Emre Nefesli <emrenefesli@outlook.com>
2023-02-21 00:44:43 +0300

    ...

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -1,6 +1,7 @@
 require("null-ls").setup({
     sources = {
         require("null-ls").builtins.formatting.shfmt, -- shell script formatting
+        require("null-ls").builtins.formatting.yamlfmt, -- shell script formatting
         -- diagnostics+actions default olarak çalışıyor shellcheck eklentisi yüklendiğinde
         -- çalışmazsa eğer ->
         --require("null-ls").builtins.diagnostics.shellcheck, -- shell script diagnostics
