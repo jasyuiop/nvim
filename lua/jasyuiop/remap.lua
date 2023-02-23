a1deb28adf53c8dcaa8d0cb705579209c653cc9e
M.Emre Nefesli <emrenefesli@outlook.com>
2023-02-24 00:45:34 +0300

    blabla

--- lua/jasyuiop/remap.lua
+++ lua/jasyuiop/remap.lua
@@ -8,7 +8,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- cursoru hareket ettirmeden J ile satır satır silme
+-- cursoru hareket ettirmeden satırları tek satırda birleştirme
 vim.keymap.set("n", "J", "mzJ`z")
 
 -- cursoru ortada tutarak ctrl + d / u ile pagede hareket etmek
@@ -23,17 +23,18 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "Q", "<nop>")
 
 -- Quickfix navigation
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+-- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+-- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+-- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+-- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- cursorun üzerinde olduğu kelimeyi tüm sayfada değiştirmeye yarıyor
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- bash script gibi dosyaları tek hamlede executable yapmaya yarıyor
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
+-- format
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- null-ls'in çalıştığı bufferlarda NullFormat komutu geçerli oluyor
@@ -43,9 +44,9 @@ vim.keymap.set("n", "<leader>nf", "<cmd>NullFormat<CR>")
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- neovim üzerinde yapılan değişiklikleri çıkmadan geçerli kılmak source .zshrc gibi.
-vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
-end)
+-- vim.keymap.set("n", "<leader><leader>", function()
+--     vim.cmd("so")
+-- end)
 
 -- Glow markdown preview
 vim.keymap.set("n", "<leader>mp", "<cmd>Glow<CR>")
