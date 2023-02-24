9583ce40cdbcf45e4205c53316b4fc1b3ca5ef10
M.Emre Nefesli <emrenefesli@outlook.com>
2023-02-24 20:03:17 +0300

    wtf3

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -2,15 +2,12 @@ local dap = require 'dap'
 local dapui = require 'dapui'
 
 require('mason-nvim-dap').setup {
-  -- Makes a best effort to setup the various debuggers with
-  -- reasonable debug configurations
   automatic_setup = false,
 
-  -- You'll need to check that you have the required things installed
-  -- online, please don't ask me how to install them :)
   ensure_installed = {
     -- Update this to ensure that you have the debuggers for the langs you want
     'delve',
+    'bashdb',
   },
 }
 
@@ -56,6 +53,7 @@ dap.listeners.before.event_exited['dapui_config'] = dapui.close
 -- Install golang specific config
 require('dap-go').setup()
 
+-- DAP BASH
 dap.adapters.bashdb = {
   type = 'executable',
   command = vim.fn.stdpath("data") .. '/mason/packages/bash-debug-adapter/bash-debug-adapter',
--- lua/jasyuiop/set.lua
+++ lua/jasyuiop/set.lua
@@ -26,6 +26,8 @@ vim.opt.undofile = true
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
+vim.o.completeopt = 'menuone,noselect'
+
 vim.opt.termguicolors = true
 
 vim.opt.scrolloff = 8
