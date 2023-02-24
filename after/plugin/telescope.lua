57d77c2d7a95641a53b1d55d7f05ef126513589e
M.Emre Nefesli <emrenefesli@outlook.com>
2023-02-24 18:08:55 +0300

    telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -3,12 +3,15 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 
 -- ctrl + p ile git files'lara bak
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+-- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 
 -- space + ps ile ripgrep kullanarak file'lar içerisinde birşey ara
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
 
+-- live grep
+vim.keymap.set('n', '<leader>pl', builtin.live_grep, {})
+
 -- help previewlere bak 
-vim.keymap.set('n', '<leader>hp', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ph', builtin.help_tags, {})
