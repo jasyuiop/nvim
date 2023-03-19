a151eed6ead819af24ad1d90914a864e2bc4f9e5
M.Emre Nefesli <emrenefesli@outlook.com>
2023-03-19 23:07:26 +0300

    nvimtree only auto open if directory

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -4,7 +4,17 @@ require("nvim-tree").setup({
   filters = { custom = { "^.git$" } }
 })
 
-local function open_nvim_tree()
+local function open_nvim_tree(data)
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+
+  -- change to the directory
+  vim.cmd.cd(data.file)
+
   -- open the tree
   require("nvim-tree.api").tree.open()
 end
