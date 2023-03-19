678c4505d7967680b51ae1f34503245c41abe924
M.Emre Nefesli <emrenefesli@outlook.com>
2023-03-19 22:54:29 +0300

    nvimtree settings

--- after/plugin/nvimtree.lua
+++ after/plugin/nvimtree.lua
@@ -1,4 +1,18 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-require("nvim-tree").setup()
+require("nvim-tree").setup({
+  filters = { custom = { "^.git$" } }
+})
+
+local function open_nvim_tree()
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
+vim.api.nvim_create_autocmd({ "QuitPre" }, {
+  callback = function() vim.cmd("NvimTreeClose") end,
+})
+
 vim.keymap.set('n', '<leader>tt', ':NvimTreeToggle<CR>')
