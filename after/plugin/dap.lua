7d308dfbfe19664d7737108918cffab6070f9e48
M.Emre Nefesli <emrenefesli@outlook.com>
2023-03-27 00:03:23 +0300

    clean unwanted lines

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -5,17 +5,13 @@ require('mason-nvim-dap').setup {
   automatic_setup = false,
 
   ensure_installed = {
-    -- Update this to ensure that you have the debuggers for the langs you want
     'delve',
     'bashdb',
   },
 }
 
--- You can provide additional configuration to the handlers,
--- see mason-nvim-dap README for more information
 require('mason-nvim-dap').setup_handlers()
 
--- Basic debugging keymaps, feel free to change to your liking!
 vim.keymap.set('n', '<F5>', dap.continue)
 vim.keymap.set('n', '<F6>', dap.step_into)
 vim.keymap.set('n', '<F7>', dap.step_over)
@@ -25,12 +21,7 @@ vim.keymap.set('n', '<leader>B', function()
   dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
 end)
 
--- Dap UI setup
--- For more information, see |:help nvim-dap-ui|
 dapui.setup {
-  -- Set icons to characters that are more likely to work in every terminal.
-  --    Feel free to remove or use ones that you like more! :)
-  --    Don't feel like these are good choices.
   icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
   controls = {
     icons = {
--- lua/jasyuiop/set.lua
+++ lua/jasyuiop/set.lua
@@ -1,43 +1,27 @@
 vim.opt.guicursor = ""
-
 vim.opt.nu = true
 vim.opt.relativenumber = true
-
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
-
 vim.opt.wrap = false
-
 vim.opt.clipboard = 'unnamedplus'
-
--- Case insensitive searching UNLESS /C or capital in search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.config/nvim/undodir"
 vim.opt.undofile = true
-
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
-
 vim.opt.termguicolors = true
-
 vim.opt.scrolloff = 8
-
 vim.opt.updatetime = 300
-
 vim.opt.colorcolumn = "80"
-
 vim.opt.signcolumn = "number"
-
 vim.opt.showmode = false
-
 vim.opt.cmdheight = 0
 
 -- [[ Highlight on yank ]]
